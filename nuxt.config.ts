b1d7f3dfb83643f851f90918d5ad933f969b5ab6
moreamora1 <felipe@moreamora.com.br>
2023-05-12 17:13:50 -0300

    ADD gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,18 @@ export default defineNuxtConfig({
         head: {
           charset: 'utf-8',
           viewport: 'width=device-width, initial-scale=1',
+          script: [
+              { src: 'https://www.googletagmanager.com/gtag/js?id=G-DFPGFGGWZG' },
+              {
+                innerHTML: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-DFPGFGGWZG');
+                `
+              }
+          ]
         }
       }
 })
