8308aa37c0475a5adeea9c3bc52e2024b13ee245
moreamora1 <felipe@moreamora.com.br>
2023-05-12 14:38:51 -0300

    ADD versao 0.0.1 do aplicativo

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <TheHeader />
+    <Hero />
   </div>
 </template>
+<script setup>
+  import TheHeader from '@/components/Header'
+  import Hero from '@/components/Hero'
+</script>
