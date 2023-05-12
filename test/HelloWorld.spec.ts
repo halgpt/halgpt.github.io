20f3d11fa22cbf8737f07555943d444b600a6951
moreamora1 <felipe@moreamora.com.br>
2023-05-12 12:22:52 -0300

    UPDATE test

--- test/HelloWorld.spec.ts
+++ test/HelloWorld.spec.ts
@@ -2,12 +2,8 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 
-//import HelloWorld from '../components/HelloWorld.vue'
-
-describe('HelloWorld', () => {
+describe('empty-test', () => {
   it('is a Vue instance', () => {
-    //const wrapper = mount(HelloWorld)
-    //expect(wrapper.vm).toBeTruthy()
     return true
   })
 })
