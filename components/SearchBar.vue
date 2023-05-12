e6bc7e38f000eb630c6bdf9125b192c922fe9acf
moreamora1 <felipe@moreamora.com.br>
2023-05-12 16:09:19 -0300

    TEST input field for iphone

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -2,7 +2,7 @@
     <form class="space-y-6 w-4/5 mt-4" action="/search" method="POST">
         <div class="mt-4">
             <input id="query" name="query" type="query" autocomplete="digite a sua busca" required 
-            class="block w-full rounded-md border-0 p-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm lg:text-lg" />
+            class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
 
         <div class="flex justify-center">
