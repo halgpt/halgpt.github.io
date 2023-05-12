f381b03e55e943c46af3fd9c1409def66cb3ac7f
moreamora1 <felipe@moreamora.com.br>
2023-05-12 16:02:49 -0300

    FIX searchbar size

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -2,7 +2,7 @@
     <form class="space-y-6 w-4/5 mt-4" action="/search" method="POST">
         <div class="mt-4">
             <input id="query" name="query" type="query" autocomplete="digite a sua busca" required 
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" />
+            class="block w-full rounded-md border-0 p-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm lg:text-lg" />
         </div>
 
         <div class="flex justify-center">
